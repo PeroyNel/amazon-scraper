57b844d49ada58d6f63a9cb2ff1cd7f6b9ad2667
Peroy Nel <peroynel@yahoo.com>
2021-12-10 14:01:43 +0200

    GET Product Details
    GET Product Details implemented

--- index.js
+++ index.js
@@ -4,10 +4,45 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = '7cdc22f1a7c755da50489d89e9183719';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Amazon Scraper API')
+});
+
+// GET Product Details
+app.get('/products/:productId', async(req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+
+    }
+
+});
+
+// GET Product Details
+app.get('/products/:productId/reviews', async(req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+
+    }
+
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
