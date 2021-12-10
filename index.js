eead97220966e14d8f34abeeb6c6da91b2062204
Peroy Nel <peroynel@yahoo.com>
2021-12-10 14:07:54 +0200

    GET Search Results
    GET Search Results implemented

--- index.js
+++ index.js
@@ -61,4 +61,20 @@ app.get('/products/:productId/offers', async(req, res) => {
 
 });
 
+// GET Search Results
+app.get('/search/:searchQuery', async(req, res) => {
+    const { searchQuery } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+
+    }
+
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
