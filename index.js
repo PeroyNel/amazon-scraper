78831ffa97ef8f27c89c9dcedb542d4bcad0c6a8
Peroy Nel <peroynel@yahoo.com>
2021-12-10 14:06:04 +0200

    GET Product Offers
    GET Product Offers implemented

--- index.js
+++ index.js
@@ -45,4 +45,20 @@ app.get('/products/:productId/reviews', async(req, res) => {
 
 });
 
+// GET Product Offers
+app.get('/products/:productId/offers', async(req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+
+    }
+
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
